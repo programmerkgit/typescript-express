9208de469c3aa6ed0ffccef4f6b86dcb4a1051c4
admin <k.kubo.private.mobile@gmail.com>
2019-06-02 01:41:44 +0900

    first

---
+++ routes/users.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+const express = require('express');
+const usersRouter = express.Router();
+
+/* GET users listing. */
+usersRouter.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+
+export { usersRouter };
