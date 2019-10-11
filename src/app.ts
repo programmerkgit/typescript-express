b191d36747203f73c9f4537c3253eba148e8ff3d
admin <k.kubo.private.mobile@gmail.com>
2019-10-11 22:03:45 +0900

    tsconfig: noImplicitAny

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { authRouter } from './app/routes/auth';
+import { ErrorRequestHandler, RequestHandler } from 'express';
 
 const createError = require('http-errors');
 const express = require('express');
@@ -28,12 +29,12 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(<RequestHandler>function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(<ErrorRequestHandler>function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
--- src/app/controller/model-controller-factory.ts
+++ src/app/controller/model-controller-factory.ts
@@ -4,75 +4,75 @@ import { NextFunction, Request, Response } from 'express-serve-static-core';
 
 const path = require('path');
 
-export const findAllFactory = (model): RequestHandler => {
+export const findAllFactory = (model: any): RequestHandler => {
     return <RequestHandler>(req: Request, res: Response, next: NextFunction) => {
         const options = req.query || {};
         const scopes = options.scopes || [];
         const connector = scopes.length ? model.scope(scopes) : model;
-        connector.findAll(options).then(instances => {
+        connector.findAll(options).then((instances: any) => {
             res.json(instances);
-        }).catch(error => {
+        }).catch((error: Error) => {
             next(error);
         });
     };
 };
 
-export const findByIdFactory = (model): RequestHandler => {
-    return <RequestHandler>(req, res, next) => {
+export const findByIdFactory = (model: any): RequestHandler => {
+    return <RequestHandler>((req, res, next) => {
         const id = req.params.id;
         const options = req.query || {};
         const scopes = options.scopes || [];
         const connector = scopes.length ? model.scope(scopes) : model;
-        connector.findById(id, options).then(instance => {
+        connector.findById(id, options).then((instance: any) => {
             if (instance) {
                 res.json(instance);
             } else {
                 console.error(`${ model.name } INSTANCE NOT FOUND`);
                 res.sendStatus(404);
             }
-        }).catch(error => {
+        }).catch((error: Error) => {
             next(error);
         });
-    };
+    });
 };
 
-export const updateFactory = (model): RequestHandler => {
+export const updateFactory = (model: any): RequestHandler => {
     return <RequestHandler>((req, res, next) => {
         const id = req.params.id;
         const options = req.query || {};
         const scopes = options.scopes || [];
         const values = req.body;
         const connector = scopes.length ? model.scope(scopes) : model;
-        connector.findById(id, options).then(instance => {
+        connector.findById(id, options).then((instance: any) => {
             if (instance) {
                 instance.update(values, {
                     fields: options.fields
-                }).then(updated => {
+                }).then((updated: any) => {
                     console.error(typeof updated.getDataValue('data'));
                     res.json(updated);
                 }).catch(next);
             } else {
                 res.sendStatus(404);
             }
-        }).catch(ValidationError, error => {
+        }).catch(ValidationError, (error: Error) => {
             res.sendStatus(403);
         }).catch(next);
     });
 };
 
-export const createFactory = (model): RequestHandler => {
+export const createFactory = (model: any): RequestHandler => {
     return <RequestHandler>((req, res, next) => {
         const options = res.locals.options;
         const values = req.body;
-        model.create(values, options).then(instance => {
+        model.create(values, options).then((instance: any) => {
             res.status(201);
             res.header('Location', path.join('/api', model.tableName.toLowerCase(), instance.id.toString()));
             res.json(instance);
-        }).catch(ConnectionError, error => {
+        }).catch(ConnectionError, (error: Error) => {
             /* ValidationError ETC */
             console.error(error);
             res.sendStatus(503);
-        }).catch(ValidationError, error => {
+        }).catch(ValidationError, (error: Error) => {
             console.error(error);
             res.sendStatus(403);
         }).catch(next);
@@ -81,10 +81,10 @@ export const createFactory = (model): RequestHandler => {
 };
 
 
-export const destroyFactory = (model): RequestHandler => {
+export const destroyFactory = (model: any): RequestHandler => {
     return <RequestHandler>((req, res, next) => {
         const id = req.params.id;
-        model.findById(id).then(instance => {
+        model.findById(id).then((instance: any) => {
             if (instance) {
                 instance.destroy().then(() => {
                     /* Should Not Show Updated Fields */
@@ -93,7 +93,7 @@ export const destroyFactory = (model): RequestHandler => {
             } else {
                 res.sendStatus(404);
             }
-        }).catch(ValidationError, error => {
+        }).catch(ValidationError, (error: Error) => {
             console.error(error);
             res.sendStatus(403);
         });
