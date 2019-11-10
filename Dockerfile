8b16657d223b5a1bc3381d568ec2ffc2b164929e
admin <k.kubo.private.mobile@gmail.com>
2019-11-10 21:14:35 +0900

    update env variables

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,11 @@ COPY ./ ./
 RUN npm install
 RUN npm run build
 ENV NODE_ENV=dev
+ENV NODE_ENV='dev'
+ENV MYSQL_USER='root'
+ENV MYSQL_PASS=''
+ENV MYSQL_HOST='127.0.0.1'
+ENV MYSQL_PORT='3306'
+ENV MYSQL_DATABASE='demo'
 EXPOSE 3000
-EXPOSE 9229
 CMD [ "npm", "run", "start" ]
--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -1,33 +1,18 @@
 const NODE_ENV = process.env.NODE_ENV || 'dev';
-const MYSQL_HOST = process.env.MYSQL_HOST || '127.0.0.1';
-const database = 'demo';
-const config = <any>{
-    'dev': {
-        'username': 'root',
-        'password': '',
-        'database': database + '_dev',
-        'host': '127.0.0.1',
-        port: 3306,
-        'dialect': 'mysql'
-    },
-    'test': {
-        'username': 'root',
-        'password': '',
-        'database': database + '_test',
-        'host': '127.0.0.1',
-        port: 3306,
-        'dialect': 'mysql'
-    },
-    'prod': {
-        'username': 'root',
-        'password': '',
-        'database': database + '_prod',
-        port: 3306,
-        'host': '127.0.0.1',
-        'dialect': 'mysql'
-    }
+const host = process.env.MYSQL_HOST || '127.0.0.1';
+const password = process.env.MYSQL_PASS || '';
+const username = process.env.MYSQL_USER || 'root';
+const port = process.env.MYSQL_PORT || '3306';
+const database = process.env.MYSQL_DATABASE || 'demo';
+export = {
+    username: username,
+    password: password,
+    database: database + '_' + NODE_ENV,
+    host: host,
+    port: port,
+    dialect: 'mysql'
 };
-export = config[ NODE_ENV ];
+
 
 // dev
 // mysql => docker + volume
