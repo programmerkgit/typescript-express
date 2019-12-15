6755f336a5fdde0d1f1cecc1262cf2e25e0b8d03
admin <k.kubo.private.mobile@gmail.com>
2019-12-16 01:03:54 +0900

    fix

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -1,33 +1,4 @@
 const NODE_ENV = process.env.NODE_ENV || 'dev';
-<<<<<<< HEAD
-const MYSQL_HOST = process.env.MYSQL_HOST || '127.0.0.1';
-const database = 'demo';
-const config = <any>{
-    'dev': {
-        'username': 'root',
-        'password': '',
-        'database': database + '_dev',
-        'host': 'localhost',
-        port: 3306,
-        'dialect': 'mysql'
-    },
-    'test': {
-        'username': 'root',
-        'password': '',
-        'database': database + '_test',
-        'host': '127.0.0.1',
-        port: 3306,
-        'dialect': 'localhost'
-    },
-    'prod': {
-        'username': 'root',
-        'password': '',
-        'database': database + '_prod',
-        port: 3306,
-        'host': '127.0.0.1',
-        'dialect': 'localhost'
-    }
-=======
 const host = process.env.MYSQL_HOST || '127.0.0.1';
 const password = process.env.MYSQL_PASS || '';
 const username = process.env.MYSQL_USER || 'root';
@@ -40,7 +11,6 @@ export = {
     host: host,
     port: port,
     dialect: 'mysql'
->>>>>>> 76c234a547a87b27965b3372908e2992882f8a19
 };
 
 
