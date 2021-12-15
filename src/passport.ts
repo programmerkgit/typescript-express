e3f67fb886c2b3a826378b6eb79ad1b973ed8f55
keishi kubo <37021071+programmerkgit@users.noreply.github.com>
2021-12-15 11:30:01 +0900

    all

--- src/passport.ts
+++ src/passport.ts
@@ -1,22 +1,22 @@
-import passport = require("passport");
-import {Strategy} from "passport-local";
-import {User} from "./db/models";
+import passport = require('passport');
+import { Strategy } from 'passport-local';
+import { User } from './db/models';
 
 
 passport.use(new Strategy({
-    usernameField: "email",
-    passwordField: "password"
+    usernameField: 'email',
+    passwordField: 'password'
 }, (email, password, done) => {
     User.findOne({where: {email: email}}).then(user => {
         if (user) {
             if (user.comparePassword(password)) {
-                console.log("Login success");
+                console.log('Login success');
                 return done(null, user);
             } else {
-                return done(null, false, {message: "Incorrect password"});
+                return done(null, false, {message: 'Incorrect password'});
             }
         } else {
-            return done(null, false, {message: "Incorrect username."});
+            return done(null, false, {message: 'Incorrect username.'});
         }
     }).catch((err: any) => {
         return done(err);
@@ -40,6 +40,6 @@ passport.deserializeUser((id: string, done) => {
 });
 
 
-export {passport};
+export { passport };
 
 
